8abf07f1faeb834e4f2aa3c2b457480e746dc704
Ngordon68 <ngordon688@gmail.com>
2025-11-16 20:45:05 -0500

    Initial Commit

---
+++ MusicKitSample/MusicKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicKitSampleApp.swift
+//  MusicKitSample
+//
+//  Created by Nick Gordon on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
